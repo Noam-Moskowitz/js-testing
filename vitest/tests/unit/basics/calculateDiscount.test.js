0b474d558f670b3aa2bde2d5f826afec7f36d676
Noam Moskowitz <noammz101@gmail.com>
2026-06-29 15:20:58 +0300

    Test file setup

---
+++ vitest/tests/unit/basics/calculateDiscount.test.js
@@ -0,0 +1,9 @@
+import {describe,it,expect} from 'vitest'
+import { isEven } from '../../../src/basics/isEven'
+
+
+describe('calculateDiscount',()=>{
+    it('should return deduct percentage from sum',()=>{
+     
+    })
+})
