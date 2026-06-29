c3587b1cbe0337d80a3fec28696649f70f71ca80
Noam Moskowitz <noammz101@gmail.com>
2026-06-29 15:27:22 +0300

    ADD: Enhance calculateDiscount tests with additional cases

--- vitest/tests/unit/basics/calculateDiscount.test.js
+++ vitest/tests/unit/basics/calculateDiscount.test.js
@@ -1,9 +1,15 @@
 import {describe,it,expect} from 'vitest'
 import { isEven } from '../../../src/basics/isEven'
+import { calculateDiscount } from '../../../src/basics/calculateDiscount'
 
 
 describe('calculateDiscount',()=>{
-    it('should return deduct percentage from sum',()=>{
-     
+    it('should deduct the percentage from the price',()=>{
+     expect(calculateDiscount(100,10)).toBe(90)
+     expect(calculateDiscount(100,50)).toBe(50)
     })
+
+    it('should not deduct if percentage is zero',()=>{
+     expect(calculateDiscount(100,0)).toBe(100)
+    });
 })
